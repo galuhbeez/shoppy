6c55fc8a8ea467aa6c4ab296b9a0f8222f9f8bc7
galuhbeez <galuhbeez@gmail.com>
2024-07-15 22:27:47 -0700

    initial commit

---
+++ selenium_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+service = webdriver.ChromeService(executable_path = './chromedriver')
+driver = webdriver.Chrome(service=service)
+driver.get("https://www.python.org")
+print(driver.title)
+
+search_bar = driver.find_element_by_name("q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
